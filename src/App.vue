c3bd61cf90f7cc405cefceb91d15afce42751ce6
exilliar <rorywebber1000@gmail.com>
2020-06-02 23:31:18 +0100

    #42 currently all broken. Turns out auth0 has shit support for typescript. Going to botch my way through it then add a shit tonne of classes for any interaction with auth0 anywhere else, also going to limit interaction as much as possible

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app>
+    <!-- Check that the SDK client is not currently loading before accessing its methods -->
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
     <Header :version="version" :user="user" />
     <v-content>
       <InputModal />
@@ -29,5 +36,15 @@ export default class App extends Vue {
   };
 
   version = "0.0.1";
+
+  login() {
+    this.$auth.loginWithRedirect();
+  }
+  // Log the user out
+  logout() {
+    this.$auth.logout({
+      returnTo: window.location.origin,
+    });
+  }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,25 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VModal from "vue-js-modal";
 
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState: any) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
+
 Vue.config.productionTip = false;
 
 Vue.use(VModal, { dynamic: true, dynamicDefaults: { clickToClose: false } });
