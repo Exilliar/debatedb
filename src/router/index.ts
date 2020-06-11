c127fafa83d00c9391d742691d032a1a74fc5b03
exilliar <rorywebber1000@gmail.com>
2020-06-11 18:25:42 +0100

    #54 moved wrapper into it's own file. Not much else to do, actually been some pretty nice code here

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import { authGuard } from "../auth/authGuard";
+import { authGuard } from "../auth/";
 
 Vue.use(VueRouter);
 
