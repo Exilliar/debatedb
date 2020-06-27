4cc61cc7fc470d0e4d30b4c9c6e41fb2e212fc1c
exilliar <rorywebber1000@gmail.com>
2020-06-27 23:57:04 +0100

    #79 it's working I think. These past 2 have been working too quickly so not sure. But I can't find any problems, so going to clean it up then make the pr

---
+++ src/db/liveClassData/quote.ts
@@ -0,0 +1,35 @@
+import QuoteTbl from "../elements/quoteTbl";
+import AxiosFuncs from "./AxiosFuncs";
+
+interface AddQuoteTbl {
+  text: string;
+  additional: string;
+  sourceid: number;
+}
+
+export default class QuoteDatadb {
+  private standardEndpoint = "source/quote/";
+  private axiosFuncs = new AxiosFuncs<QuoteTbl, AddQuoteTbl>();
+
+  async getAll(sourceid: number): Promise<QuoteTbl[]> {
+    return await this.axiosFuncs.getAll("source/" + sourceid + "/quote");
+  }
+  async getSingle(quoteid: number): Promise<QuoteTbl> {
+    return await this.axiosFuncs.getSingle(this.standardEndpoint + quoteid);
+  }
+  async add(newQuote: AddQuoteTbl): Promise<number> {
+    return await this.axiosFuncs.add(
+      "source/" + newQuote.sourceid + "/quote",
+      newQuote,
+    );
+  }
+  async update(updateQuote: QuoteTbl): Promise<void> {
+    return await this.axiosFuncs.update(
+      this.standardEndpoint + updateQuote.id,
+      updateQuote,
+    );
+  }
+  async delete(quoteid: number): Promise<void> {
+    return await this.axiosFuncs.delete(this.standardEndpoint + quoteid);
+  }
+}
